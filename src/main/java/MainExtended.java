f46dbc1668268bb8a080e6045fb4c874f31d9f12
Agnieszka Pióro <pioro-agnieszka@wp.pl>
2021-06-28 22:28:52 +0200

    Zadanie2

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Euro2020";
+        int countries = 24;
+        double cities = 11;
+        char polandGroup = 'E';
+        boolean isPolandWinEuro = false;
+        boolean isSpainWinEuro = true;
+        System.out.println(name);
+        System.out.println(countries);
+        System.out.println(cities);
+        System.out.println(polandGroup);
+        System.out.println(isPolandWinEuro);
+        System.out.println(isSpainWinEuro);
+    }
+}
