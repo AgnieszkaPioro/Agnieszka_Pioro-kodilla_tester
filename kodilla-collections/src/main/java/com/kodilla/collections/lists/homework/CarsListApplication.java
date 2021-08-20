bc24421f03bfefa5dd9318afc3eadd9625218da6
Agnieszka Pióro <pioro-agnieszka@wp.pl>
2021-08-20 12:22:54 +0200

    Zadanie 4.3 poprawione

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -3,6 +3,8 @@ package com.kodilla.collections.lists.homework;
 import com.kodilla.collections.arrays.homework.CarUtils;
 import com.kodilla.collections.interfaces.homework.Car;
 import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Lexus;
+import com.kodilla.collections.interfaces.homework.Opel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,20 +12,24 @@ import java.util.List;
 public class CarsListApplication {
 
     public static void main(String[] args) {
-        List<Ford> cars = new ArrayList<>();
-        Ford fordAga = new Ford(27);
+        List<Car> cars = new ArrayList<>();
+        Car fordAga = new Ford(29);
+        Car lexusAga = new Lexus(80);
+        Car opelAga = new Opel(40);
         cars.add(fordAga);
-        cars.add(new Ford(80));
-        cars.add(new Ford(40));
+        cars.add(lexusAga);
+        cars.add(opelAga);
+
+        for (Car car : cars)  {
+            CarUtils.describeCar(car);
+        }
 
         cars.remove(2);
         cars.remove(fordAga);
 
-        System.out.println(cars.size());
 
-        for (Car ford : cars) {
-            CarUtils.describeCar(ford);     //wyświetl w pętli for-each zawartość kolekcji, używając przygotowanej wcześniej metody describeCar z klasy CarUtils
-
+        for (Car car : cars) {
+            CarUtils.describeCar(car);
         }
     }
 }
