015b8a1dddd13657ff074645bf76792a64ddb493
Agnieszka Pióro <pioro-agnieszka@wp.pl>
2021-08-24 20:53:03 +0200

    Zadanie 5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/HomeworkExample.java
@@ -0,0 +1,13 @@
+//package com.kodilla.collections.adv.immutable.homework;
+//
+//import com.kodilla.collections.adv.immutable.BookHacked;
+//
+//public class HomeworkExample {
+//
+//    public static void main(String[] args) {
+//        Task task = new TaskHacked("Tomato",11);
+//        TaskHacked taskHacked = (TaskHacked) task;
+//        taskHacked.modifyTitle("Cucumber");
+//        System.out.println(task.getTitle());
+//    }
+//}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskHacked.java
@@ -0,0 +1,19 @@
+//package com.kodilla.collections.adv.immutable.homework;
+//
+//public class TaskHacked extends Task {
+//
+//    private String realTitle;
+//
+//    public TaskHacked(String title, int duration) {
+//        super(title, duration);
+//        realTitle = title;
+//    }
+//    @Override
+//    public String getTitle() {
+//        return realTitle;
+//    }
+//    public void modifyTitle(String newTitle) {
+//        realTitle = title;
+//
+//    }
+//}
