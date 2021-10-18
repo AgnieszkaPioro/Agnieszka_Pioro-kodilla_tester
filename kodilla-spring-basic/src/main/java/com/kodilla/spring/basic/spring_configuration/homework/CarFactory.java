58cc8a066ff677f5c01d5fbdb0eef6fa4bb3106e
Agnieszka Pióro <pioro-agnieszka@wp.pl>
2021-10-18 21:24:27 +0200

    Zadanie 9.4 poprawione

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -3,7 +3,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Month;
 
 @Configuration
 public class CarFactory {
@@ -26,21 +28,25 @@ public class CarFactory {
     public Car createNewCar() {
 
         Car car;
+        LocalDate localDate = LocalDate.now();
+        LocalDate localDateStartSummer = LocalDate.of(2021, 6, 22);
+        LocalDate localDateStartFall = LocalDate.of(2021, 9, 23);
+        LocalDate localDateStartWinter = LocalDate.of(2021, 12, 22);
+        LocalDate localDateStartSpring = LocalDate.of(2021, 3, 21);
 
-        if (choosenSeason == "summer") {
+
+        if (localDate.isBefore(localDateStartFall) && localDate.isAfter(localDateStartSummer)) {
             car = new Cabrio();
 //            System.out.println("A car was created " + car);
-        }
-        else if (choosenSeason == "spring") {
+        } else if (localDate.isBefore(localDateStartSummer) && localDate.isAfter(localDateStartSpring)) {
             car = new Sedan();
 //            System.out.println("A car was created " + car);
-        }
-        else if (choosenSeason == "fall") {
+        } else if (localDate.isBefore(localDateStartWinter) && localDate.isAfter(localDateStartFall)) {
             car = new Sedan();
 //            System.out.println("A car was created " + car);
-        }
-        else
+        } else {
             car = new SUV();
+        }
 
         return car;
     }
